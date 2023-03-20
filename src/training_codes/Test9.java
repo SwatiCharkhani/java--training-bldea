52b5f94a3fd092e048368bf63e2e9c1df545668a
swati <swati@Swati2002>
2023-03-20 21:12:51 +0530

    Ninth commit

--- src/training_codes/Test9.java
+++ src/training_codes/Test9.java
@@ -1,10 +1,24 @@
 package training_codes;
 
+import java.util.Scanner;
+
 public class Test9 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner sc=new Scanner(System.in);
+		 System.out.println("enter the age:");
+		 int age=sc.nextInt();
+		 if(age<18) {
+			 System.out.println("you are still minor");
+		 }
+		 else if(age>65) {
+			 System.out.println("you are senoir citizen");
+		 }
+		 else
+		 {
+			 System.out.println("Congrajulations! you are most eligible groom");
+		 }
 	}
 
 }
