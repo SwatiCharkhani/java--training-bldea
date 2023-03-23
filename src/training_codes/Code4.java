8b573ca46c4d181e5c702a06dfd582eff0bf3c59
swati <swati@Swati2002>
2023-03-23 19:06:00 +0530

    14th commit

---
+++ src/training_codes/Code4.java
@@ -0,0 +1,16 @@
+package training_codes;
+
+import java.util.Arrays;
+
+public class Code4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {10,20,30,40,50,60,70,80,90,100};
+		 Arrays.sort(arr);
+		 for(int i=0;i<arr.length;i++) {
+			 System.out.print(arr[i]+",");
+		 }
+	}
+
+}
