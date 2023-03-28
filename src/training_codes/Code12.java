f18816a7eeaa959478a917b78ddd3c0b83a84e26
swati <swati@Swati2002>
2023-03-28 21:14:33 +0530

    Non constant Pool-Address

---
+++ src/training_codes/Code12.java
@@ -0,0 +1,18 @@
+package training_codes;
+
+public class Code12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1= new String ("RAMA");
+		 String s2=new String ("RAMA");
+		 if(s1==s2) {
+			 System.out.println("Addresses are Equal");
+			 
+		 }
+			 else {
+				 System.out.println("Addresses are not equal");
+			 }
+	}
+
+}
