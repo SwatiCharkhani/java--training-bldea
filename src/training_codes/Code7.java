8d9aff903efcb27a7cf5ccbb98d8dcb72ed44289
swati <swati@Swati2002>
2023-03-28 20:31:57 +0530

    Palindrome

---
+++ src/training_codes/Code7.java
@@ -0,0 +1,25 @@
+package training_codes;
+
+import java.util.Scanner;
+
+public class Code7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		  System.out.println("Enter the string:");
+String str1=sc.next();
+String str2="";
+for(int i=str1.length()-1;i>=0;i--) {
+	  str2=str2+str1.charAt(i);
+}
+if(str1.equals(str2)==true) {
+	  System.out.println("Palindrome");
+}
+else {
+	  System.out.println("Not Palindrome");
+}
+
+	}
+
+}
