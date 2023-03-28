1024f8c446862c6506b9cf66ae0dd97439603239
swati <swati@Swati2002>
2023-03-28 20:28:51 +0530

    Replace @ in place of vowel

---
+++ src/training_codes/Code6.java
@@ -0,0 +1,28 @@
+package training_codes;
+import java.util.Scanner;
+public class Code6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method studio
+				Scanner sc=new Scanner(System.in);
+				  System.out.println("Enter the string:");
+		String str1=sc.next();
+		str1=str1.toLowerCase();
+		for(int i=0;i<str1.length();i++) {
+			if(str1.charAt(i)=='a' ||str1.charAt(i)=='e'|| str1.charAt(i)=='i' ||str1.charAt(i)=='o' || str1.charAt(i)=='u') {
+				System.out.print("@");
+			}
+
+		else {
+			
+			System.out.print(str1.charAt(i));
+		}
+		}
+
+
+			}
+		
+		                
+	}
+
+
