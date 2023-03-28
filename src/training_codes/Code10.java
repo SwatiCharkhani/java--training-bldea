1387f57a5cab1c9f02b59f30551cb96feb36d1f7
swati <swati@Swati2002>
2023-03-28 20:41:05 +0530

    Constant Pool Strings

---
+++ src/training_codes/Code10.java
@@ -0,0 +1,18 @@
+package training_codes;
+
+public class Code10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="RAMA";
+		 String s2="RAMA";
+		 if(s1.equals(s2)) {
+			 System.out.println("Strings equal");
+			 
+		 }
+			 else {
+				 System.out.println("Strings are not equal");
+			 }
+	}
+
+}
