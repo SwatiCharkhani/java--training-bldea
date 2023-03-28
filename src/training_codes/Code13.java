adbcb5a178b367b07a3e581a490f2c6fec24ac6a
swati <swati@Swati2002>
2023-03-28 21:17:12 +0530

    Non constant Pool-strings

---
+++ src/training_codes/Code13.java
@@ -0,0 +1,18 @@
+package training_codes;
+
+public class Code13 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1= new String ("RAMA");
+		 String s2=new String ("RAMA");
+		 if(s1.equals(s2)) {
+			 System.out.println("Strings are Equal");
+			 
+		 }
+			 else {
+				 System.out.println("Strings are not equal");
+			 }
+	}
+
+}
