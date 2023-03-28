e5ceca10981c86c2e181ff2b99f765d47228ed31
swati <swati@Swati2002>
2023-03-28 20:47:46 +0530

    Constant Pool- Address

---
+++ src/training_codes/Code11.java
@@ -0,0 +1,18 @@
+package training_codes;
+
+public class Code11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s1="RAMA";
+		 String s2="RAMA";
+		 if(s1==s2) {
+			 System.out.println("Addresses are Equal");
+			 
+		 }
+			 else {
+				 System.out.println("Addresses are not equal");
+			 }
+	}
+
+}
